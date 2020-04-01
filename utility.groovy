e45755902967cb6cfcffd404deaa47d0ac181aea
Kunal <kunaljha5@gmail.com>
2020-04-02 00:23:05 +0530

    Update utility.groovy

--- utility.groovy
+++ utility.groovy
@@ -27,3 +27,24 @@ def getEnv (stage, environment){
         }
     }
 }
+
+setupHelm(helmVersion) {
+    if(helmVersion) {
+        sh(returnStdout: true,
+           script: """
+             echo "getting helm"
+             curl -o helm.tar.gz -ks "https://get.helm.sh/helm-${helmVersion}-linux-arm.tar.gz" && \\
+             tar -xzvf helm.tar.gz linux-amd64/helm -C . && \\
+             rm -rf helm.tar.gz && \\
+             mv linux-amd64/helm /usr/bin/helm && \\
+             chmod +x /usr/bin/helm && \\
+             echo "helm setup completed"
+           """)
+    }
+}
+
+helmVersion="v2.16.5"
+setupHelm(helmVersion)
+
+
+
